fb9728a8e812af1f5b2dcf6281ea050502695b63
kk.ogura <kk.ogura@satis-corp.com>
2021-05-12 20:44:31 +0900

    繰り返し取得実装

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
 # get_harvest_info_xem
 
+XEMのハーベスト履歴を取得してCSV出力するツールです。
+自分用に作ったのでバグがあるかもしれませんが、シンプルな内容なので簡単にお使い頂けると思います。
+
+## 前提(premise)
+
+* `python3`コマンドが実行可能
+* MacOSでのみ動作確認しました。
+
+## 準備(preparation)
+
 $ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 $ python3 get-pip.py
-$ pip install requests
+$ pip3 install requests
+
+## 実行(execution)
+
+$ git clone https://github.com/kkogr/get_harvest_info_xem.git
+$ cd get_harvest_info_xem
 $ python3 get_harvest_info_xem.py [address]
+
+## 結果(output)
+
+以下のような内容のharvest_info_xem.csvというcsvファイルが生成されます。
+
+```
+date,amount
+2021/05/02 00:25:37,0.0
+2021/04/21 00:19:17,0.0
+2021/04/19 22:56:37,4.05
+・
+・
+・
+```
--- get_harvest_info_xem.py
+++ get_harvest_info_xem.py
@@ -6,16 +6,23 @@ import requests
 import json
 import datetime
 
+if len(sys.argv) != 2:
+    print('Usage:')
+    print('  python3 get_harvest_info_xem.py [your_xem_address]')
+    sys.exit(1)
+
 args = sys.argv
 address = args[1]
-print (address)
 
-# url
-url = "http://alice6.nem.ninja:7890/account/harvests?" + "address" + "=" + address
-print (url)
+# baseUrl
+baseUrl = "http://go.nem.ninja:7890/account/harvests?" + "address" + "=" + address
 
 # get request
-response = requests.get(url)
+response = requests.get(baseUrl)
+if response.status_code != requests.codes.ok:
+    print("status code:" + str(response.status_code))
+    print("Make sure the address you specified is correct.")
+    sys.exit(1)
 
 # response.json()でJSONデータに変換して変数へ保存
 jsonData = response.json()
@@ -23,8 +30,33 @@ jsonData = response.json()
 # ネメシスブロック日時
 nemesisBlockTime = 1427587585
 
-for jsonObj in jsonData['data']:
-    timestamp = nemesisBlockTime + jsonObj['timeStamp']
-    date = datetime.datetime.fromtimestamp(timestamp, datetime.timezone(datetime.timedelta(hours=9))).strftime("%Y/%m/%d %H:%M:%S")
-    amount = str(jsonObj['totalFee'] / 1000000)
-    print(date + "," + amount)
+# ハーベスト回数
+harvestCount = 0
+
+# 出力csvのヘッダ記載
+if jsonData['data'] != []:
+    f = open('harvest_info_xem.csv', 'w', encoding='UTF-8')
+    f.write('date,amount\n')
+
+# 要素数が0で無い場合に繰り返す
+while jsonData['data'] != []:
+    # 要素数分繰り返す
+    harvestCount += len(jsonData['data'])
+    # 最後の要素のidを記録するための変数
+    last_id = 0
+    for jsonObj in jsonData['data']:
+        timestamp = nemesisBlockTime + jsonObj['timeStamp']
+        date = datetime.datetime.fromtimestamp(timestamp, datetime.timezone(datetime.timedelta(hours=9))).strftime("%Y/%m/%d %H:%M:%S")
+        amount = str(jsonObj['totalFee'] / 1000000)
+        f.write(date + "," + amount + '\n')
+        # 要素のidを記録
+        last_id = str(jsonObj['id'])
+    # 最後の要素のidをqueryに追加して再度get request
+    url = baseUrl + "&" + "id" + "=" + last_id
+    # get request
+    response = requests.get(url)
+    # response.json()でJSONデータに変換して変数へ保存
+    jsonData = response.json()
+
+f.close()
+print("Done.")
